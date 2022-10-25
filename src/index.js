dafab540adccabf7ee356266e6660d07e8b0e89a
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-10-25 13:19:58 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// HTTP Logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  })
+);
+
+app.set(
+  'view engine',
+  
+  'hbs');
+app.set('views', path.join(__dirname, '/resources/views'));
+
+// Routes init
+route(app);
+
+app.listen(port, () => console.log('Listening...!'));
